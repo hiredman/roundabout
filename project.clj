c600923137668c833a864a88905a93bb0bdf0427
Kevin Downey <hiredman@thelastcitadel.com>
2017-07-29 00:38:03 -0700

    bump core.async deps

--- project.clj
+++ project.clj
@@ -3,9 +3,11 @@
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.2.374"]]
+                 [org.clojure/core.async "0.3.443"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
-                                  [org.clojure/clojurescript "1.7.170"]]}}
+                                  [org.clojure/clojurescript "1.7.170"]]}
+             :clj19 {:dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                                    [org.clojure/spec.alpha "0.1.108"]]}}
   :cljsbuild {:builds [{:source-paths ["src"]
                         :compiler {:output-to "main.js"
                                    :optimizations :none
