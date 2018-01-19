3995ffc4630391fa7e682beb5e0c4d9a124b4079
Kevin Downey <hiredman@thelastcitadel.com>
2018-01-18 21:50:22 -0800

    bump deps

--- project.clj
+++ project.clj
@@ -2,12 +2,10 @@
   :description "A library for flow control"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.3.443"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.474"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
-                                  [org.clojure/clojurescript "1.7.170"]]}
-             :clj19 {:dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                                    [org.clojure/spec.alpha "0.1.108"]]}}
+                                  [org.clojure/clojurescript "1.7.170"]]}}
   :cljsbuild {:builds [{:source-paths ["src"]
                         :compiler {:output-to "main.js"
                                    :optimizations :none
